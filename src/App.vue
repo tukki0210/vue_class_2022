fc46041cdb343e03b348cd3bc26655b2e94cf03b
tukki0210 <tukki0210@gmail.com>
2022-12-21 13:26:55 +0900

    hotelAPIサンプル

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,51 @@
 <template>
-  <BookList v-bind:title="title" v-bind:url="ImageUrl" />
-  <BookCard v-bind:bookData = "bookData" />
-  <!-- コンポーネントへのv-forの場合、複製されたコンポーネントを区別する重複しないkey属性が必要 -->
   <BookCard v-for="book in books"
             v-bind:bookData = "book.Item" 
             v-bind:key="book.Item.isbn" />
-
-  <!-- P.88 v-forから配列要素だけでなく、その配列の各キーやインデックス番号が取れる -->
-  <BookCard v-for="(book, key,index ) in books"
-            v-bind:bookData = "book.Item" 
-            v-bind:key="index" />
+            
+  <HotelCard v-for="(hotel,key,index) in hotels" 
+              v-bind:hotel = "hotel.hotel"
+              v-bind:key="index" />
 </template>
 
 <script>
-// import 外部ファイルを読み込む
-import BookList from './components/BookList.vue'
 import BookCard from './components/BookCard.vue'
+import HotelCard from './components/HotelCard.vue'
 
-// export 外部ファイルに公開する
 export default {
   name: 'App',
   components: {
-    BookList,
-    BookCard
+    BookCard,
+    HotelCard
   },
   data () {
     return {
-    // 初回ロード時に渡すダミーデータ
-     title:'あああ',
-     ImageUrl:'',
-     bookData : {}, //空のオブジェクト
      books : [], // 空の配列
+     hotels :[]
     }
   },
-  // ライフサイクルフック（実行タイミングの指定）
-  // 非同期処理を含む関数はmountedの中にに書く
   mounted: function () {
-    const RAKUTEN_API = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=programing&booksGenreId=001&applicationId=1024837784734370415'
+    const RAKUTEN_API = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&title=javascript&booksGenreId=001&applicationId=1024837784734370415'
     fetch(RAKUTEN_API)
       .then(response => response.json())
       .then(data => {
+        console.log(data)
         // 複数の本のデータが入った配列
         const books = data.Items
         this.books = books
 
-        // bookオブジェクトに指定した本の全データが入ってる
-        const book = data.Items[1].Item
-        // 非同期処理で取得したデータは、そのまま他のコンポーネントに渡せない
-        // 本の名前を取得し、data()のtitleにコピー
-        this.title = book.title
-        this.ImageUrl = book.mediumImageUrl
+      })
 
-        // 値の代わりにオブジェクトを渡す
-        this.bookData = book
+    const RAKUTEN_HOTEL_API = "https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?format=json&largeClassCode=japan&middleClassCode=nara&smallClassCode=nara&applicationId=1024837784734370415"
+    fetch(RAKUTEN_HOTEL_API)
+      .then(res => res.json())
+      .then(data => {
+        // 取れているか確認
+        console.log(data)
+        // 複数のホテルのデータが入った配列
+        const hotels = data.hotels
+        // data()のhotelsにコピー
+        this.hotels = hotels
       })
     }
 }
---
+++ src/components/HotelCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <img v-bind:src="bookData.mediumImageUrl" alt="">
+        <div>
+            <div>{{ bookData.title }}</div>
+            <div>{{ bookData.publisherName }}</div>
+            <div>{{ bookData.itemPrice }}</div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BookCard',
+    props: {
+        bookData: Object
+    }
+}
+</script>
+<style>
+.container {
+    display: flex;
+    flex-direction: row;
+    border: solid 1px gray;
+    margin: 0;
+    padding: 5px;
+    width: 500px;
+}
+</style>
